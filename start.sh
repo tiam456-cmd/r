8cd6ddba40949fd98f6c0768c165401679d6e201
tiam456-cmd <talhatjaw7@gmail.com>
2025-07-31 19:48:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start Xvfb on display :99 in the background
-Xvfb :99 -screen 0 1024x768x24 &
+Xvfb :98 -screen 0 1024x768x24 &
 
 # Export DISPLAY so GUI apps use the virtual display
-export DISPLAY=:99
+export DISPLAY=:98
 
 # Run your FastAPI app with uvicorn
 exec uvicorn gpt:app --host 0.0.0.0 --port 8000
